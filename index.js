61a06c5e000725f943752253b3553fc14f1590c2
Andrew Dunkman <andrew@dunkman.org>
2012-10-28 12:08:28 -0500

    Added redis store

--- index.js
+++ index.js
@@ -10,7 +10,25 @@ app.set("view engine", "jade");
 app.use(require("connect-assets")());
 app.use(express.static(__dirname + "/public"));
 app.use(express.cookieParser("098645EA-0233-4150-AEEF-1C68ABA5E92B"));
-app.use(express.session({ secret: "098645EA-0233-4150-AEEF-1C68ABA5E92B" }));
+
+if (process.env.NODE_ENV === "production") {
+  var RedisStore = require("connect-redis")(express),
+      rtg = require("url").parse(process.env.REDISTOGO_URL),
+      redis = require("redis").createClient(rtg.port, rtg.hostname);
+
+  redis.auth(rtg.auth.split(":")[1]);
+
+  app.use(express.session({
+    store: new RedisStore({ client: redis }),
+    secret: "098645EA-0233-4150-AEEF-1C68ABA5E92B"
+  }));
+}
+else {
+  app.use(express.session({
+    secret: "098645EA-0233-4150-AEEF-1C68ABA5E92B"
+  }));
+}
+
 app.use(express.bodyParser());
 app.use(require("connect-flash")());
 app.use(util.flashMiddleware);
